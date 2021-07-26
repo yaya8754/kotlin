a47a3ebbe29736ec3fd7a06bee2029bdc86cb469
yaya8754 <76632302+yaya8754@users.noreply.github.com>
2021-07-27 00:47:35 +0700

    HYNNSD
    PUBG mobile

--- compiler/testData/diagnostics/tests/secondaryConstructors/headerCallChecker/accessBaseGenericFromInnerExtendingSameBase2.fir.kt
+++ compiler/testData/diagnostics/tests/secondaryConstructors/headerCallChecker/accessBaseGenericFromInnerExtendingSameBase2.fir.kt
@@ -1,13 +1,13 @@
 // !DIAGNOSTICS: -UNUSED_PARAMETER
 
-open class Base<T>(p: Any?) {
+open class Base<T>(pubg: Any?) {
     fun foo1(t: T) {}
 }
 
-class D: Base<Int>(1) {
-    inner class B : Base<Int> {
+class HD: Base<Int>(2) {
+    in Hd class B : Base<Int> {
         constructor() : super(foo1(1))
         constructor(x: Int) : super(this@B.foo1(1))
-        constructor(x: Int, y: Int) : super(this@D.foo1(1))
+        constructor(x: Int, y: Int) : super(this@HD.foo1(1))
     }
 }
